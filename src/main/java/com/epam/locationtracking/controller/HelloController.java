f2a15126d259faee5c316fd52e6cb43f2db3268b
Bohdan Kolesnyk <bkolesnyk85@gmail.com>
2015-06-09 21:46:56 +0300

    Issue1: application context error

--- src/main/java/com/epam/locationtracking/controller/HelloController.java
+++ src/main/java/com/epam/locationtracking/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.epam.locationtracking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epam.locationtracking.service.impl.EmailConfirmationService;
+
 @RestController
 public class HelloController {
+	
+	@Autowired
+	EmailConfirmationService emailService;
 
     @RequestMapping("/")
     public String index() {
+    	emailService.send("Bohdan", "bkolesnyk85@gmail.com", "http:www.google.com");
         return "Greetings from Spring Boot!";
     }
 
